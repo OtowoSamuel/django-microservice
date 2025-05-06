aa6f98f94895236bd7cab7d1a08852ad21a876c7
OtowoSamuel <samuelotowo@gmail.com>
2025-05-06 16:01:34 +0100

    Added deleteresources.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,24 @@ resource "aws_default_vpc" "default" {}
 
 # Data source to reference the existing default subnet in us-east-1a
 data "aws_subnet" "existing_default_a" {
-  id = "subnet-05c1fad9d625903e9" 
+  id = "subnet-05c1fad9d625903e9" # Existing subnet ID from the error
 }
 
 # Create a default subnet in us-east-1b (if it doesn't exist)
 resource "aws_default_subnet" "default_b" {
   availability_zone = "us-east-1b"
+}
+
+# Data source to reference the existing eks-nodes-sg security group
+data "aws_security_group" "eks_nodes" {
+  filter {
+    name   = "group-name"
+    values = ["eks-nodes-sg"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
 }
 
 resource "aws_security_group" "db" {
@@ -25,7 +37,7 @@ resource "aws_security_group" "db" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes.id]
+    security_groups = [data.aws_security_group.eks_nodes.id]
   }
 
   ingress {
@@ -33,33 +45,13 @@ resource "aws_security_group" "db" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes.id]
+    security_groups = [data.aws_security_group.eks_nodes.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "eks_nodes" {
-  name        = "eks-nodes-sg"
-  description = "Allow EKS nodes access"
-  vpc_id      = aws_default_vpc.default.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8000
-    to_port     = 8000
-    cidr_blocks = ["0.0.0.0/0"] # Restrict to your CIDR range in production
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
