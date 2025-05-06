280ca09c45d02a4d08f6be3e4c7fe820de1f395a
OtowoSamuel <samuelotowo@gmail.com>
2025-05-06 16:04:25 +0100

    Added deleteresources.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_default_vpc" "default" {}
 
 # Data source to reference the existing default subnet in us-east-1a
 data "aws_subnet" "existing_default_a" {
-  id = "subnet-05c1fad9d625903e9" # Existing subnet ID from the error
+  id = "subnet-05c1fad9d625903e9" # Existing subnet ID
 }
 
 # Create a default subnet in us-east-1b (if it doesn't exist)
@@ -27,39 +27,23 @@ data "aws_security_group" "eks_nodes" {
   }
 }
 
-resource "aws_security_group" "db" {
-  name        = "allow-postgres-redis"
-  description = "Allow PostgreSQL and Redis inbound traffic"
-  vpc_id      = aws_default_vpc.default.id
-
-  ingress {
-    description     = "PostgreSQL from EKS nodes"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [data.aws_security_group.eks_nodes.id]
+# Data source to reference the existing allow-postgres-redis security group
+data "aws_security_group" "db" {
+  filter {
+    name   = "group-name"
+    values = ["allow-postgres-redis"]
   }
-
-  ingress {
-    description     = "Redis from EKS nodes"
-    from_port       = 6379
-    to_port         = 6379
-    protocol        = "tcp"
-    security_groups = [data.aws_security_group.eks_nodes.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
   }
 }
 
-resource "aws_ecr_repository" "app" {
+# Data source to reference the existing django-microservice ECR repository
+data "aws_ecr_repository" "app" {
   name = "django-microservice"
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
+  value = data.aws_ecr_repository.app.repository_url
 }
